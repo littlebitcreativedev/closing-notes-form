33a5b2a630c1c1ad8c51f99bafef571a8e39d753
littlebitcreativedev <littlebitcreativedev@gmail.com>
2026-04-30 19:11:05 -0600

    change form submitting api from nocodeapi to a direct google sheets and netlify api connection

---
+++ netlify/functions/submit-form.js
@@ -0,0 +1,39 @@
+const { google } = require("googleapis")
+
+exports.handler = async (event) => {
+    if (event.httpMethod !== "POST") {
+        return { statusCode: 405, body: "Method Not Allowed"}
+    }
+
+    try {
+        const body = JSON.parse(event.body)
+
+        const auth = new google.auth.JWT({
+            email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+            key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+            scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+        })
+
+        const sheets = google.sheets({ version: "v4", auth})
+
+        await sheets.spreadsheets.values.append({
+            spreadsheetId: process.env.GOOGLE_SHEET_ID,
+            range: "FormResponses!A1",
+            valueInputOption: "USER_ENTERED",
+            requestBody: {
+                values: rows
+            },
+        })
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ success: true})
+        }
+    } catch (err) {
+        console.error(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Submission Failed."})
+        }
+    }
+}
