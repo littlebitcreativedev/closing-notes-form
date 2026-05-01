2be402e5bc7127db06c5ebced4753a32f9d403cd
littlebitcreativedev <littlebitcreativedev@gmail.com>
2026-04-30 21:21:53 -0600

    mmmm

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -177,11 +177,9 @@ const IndexPage = () => {
         if (isBrowser) {
           try{
             const response = await fetch('https://script.google.com/macros/s/AKfycbwDt4RSdXDLibRZguEm1mpiHlsfjfc3gphZYK2a3RKHANTGqOrQMRAGslYaBSUijMFo/exec', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(formattedData)
+                  method: 'POST',
+                  headers: { 'Content-Type': 'application/json' },
+                  body: JSON.stringify(formattedData)
             })
 
             const result = await response.json()
