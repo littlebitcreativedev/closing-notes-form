d835aecf8cfe1e56ddf55986089bdf705e19766c
littlebitcreativedev <littlebitcreativedev@gmail.com>
2026-04-30 21:17:34 -0600

    blah

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -176,7 +176,7 @@ const IndexPage = () => {
 
         if (isBrowser) {
           try{
-            const response = await fetch('/.netlify/functions/submit-form', {
+            const response = await fetch('https://script.google.com/macros/s/AKfycbwDt4RSdXDLibRZguEm1mpiHlsfjfc3gphZYK2a3RKHANTGqOrQMRAGslYaBSUijMFo/exec', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json'
